16979ca4e73b2f4dc0597726227bbc12358c5864
Pratik Boghani <53119774+pratikboghani@users.noreply.github.com>
2023-11-15 15:19:34 -0500

    Home page design

--- index.php
+++ index.php
@@ -1,14 +1,35 @@
+<?php
+    
+    ?>
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Comics - Home</title>
-</head>
-
-<body>
-    Home Page
-</body>
-
+<html>
+    <head>
+        <title>
+            Comics Store
+        </title>
+        <link rel="stylesheet" href="public/css/index.css" type="text/css">
+    </head>
+    <body>
+        <header>
+            Comics Store
+        </header>
+        <nav>
+            <div class="container">
+                <ul>
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="list.php">Comics</a></li>
+                    <li><a href="insert.php">Cart</a></li>
+                    <li><a href="insert.php">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container">
+            <h2>Welcome to Comics Store</h2>
+        </div>
+        <footer>
+            <div class="container">
+                 ~Developed by Duel Ninja - Comics Group
+            </div>
+        </footer>
+    </body>
 </html>
