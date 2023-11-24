617a43a1458258c4ca87c7b9bb8a1ecc435c7a69
Ankit Maniya <66209504+ankit-maniya@users.noreply.github.com>
2023-11-24 01:38:35 -0500

    fixies home page navbar uri

--- pages/components/navbar.php
+++ pages/components/navbar.php
@@ -1,7 +1,7 @@
 <nav>
     <div class="container">
         <ul>
-            <li class="<?php echo $activeTab == "" ? "active" : ""; ?>"><a href="../index.php">Home</a></li>
+            <li class="<?php echo $activeTab == "" ? "active" : ""; ?>"><a href="<?php echo Path::getDomainUri(); ?>">Home</a></li>
             <li class="<?php echo $activeTab == "comics" ? "active" : ""; ?>"><a href="<?php echo Path::getDomainUri(); ?>pages/comics.php">Comics</a></li>
             <li class="<?php echo $activeTab == "cart" ? "active" : ""; ?>"><a href="<?php echo Path::getDomainUri(); ?>pages/cart.php">Cart</a></li>
             <li class='<?php echo $activeTab == "login" ? "active" : ""; ?>'><a href="<?php echo Path::getDomainUri(); ?>pages/login.php">Login</a></li>
