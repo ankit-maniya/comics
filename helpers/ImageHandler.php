5baa7d94dd92e1995ed28bb304c10eac5fca0c56
Ankit Maniya <66209504+ankit-maniya@users.noreply.github.com>
2023-11-28 14:29:09 -0500

    removed unwanted comments

--- helpers/ImageHandler.php
+++ helpers/ImageHandler.php
@@ -10,44 +10,35 @@ class ImageHandler
             mkdir($targetDir, 0755, true);
         }
 
-        // Generate a unique filename
         $timestamp = time();
-        $randomString = bin2hex(random_bytes(8)); // Adjust the number of bytes for your needs
+        $randomString = bin2hex(random_bytes(8));
         $uniqueFilename = $timestamp . "_" . $randomString;
 
-        // Get the file extension
         $imageFileType = strtolower(pathinfo($file["name"], PATHINFO_EXTENSION));
 
-        // Combine the unique filename with the original file extension
         $targetFile = $targetDir . $uniqueFilename . "." . $imageFileType;
         $uploadOk = 1;
 
-        // Check if the file is an actual image
         $check = getimagesize($file["tmp_name"]);
         if ($check !== false) {
-            // echo "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
         } else {
             return "File is not an image.";
         }
 
-        // Check if file already exists
         if (file_exists($targetFile)) {
             return "Sorry, file already exists.";
         }
 
-        // Check file size (you can adjust the size as needed)
         if ($file["size"] > 5000000000000) {
             return "Sorry, your file is too large.";
         }
 
-        // Allow only certain file formats
         $allowedFormats = ["jpg", "jpeg", "png", "gif"];
         if (!in_array($imageFileType, $allowedFormats)) {
             return "Sorry, only JPG, JPEG, PNG, and GIF files are allowed.";
         }
 
-        // Check if $uploadOk is set to 0 by an error
         if ($uploadOk == 0) {
             return "Sorry, your file was not uploaded.";
         } else {
