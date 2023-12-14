d9200f7f40f0b5e5d2e37b1815940bb18750601b
Zeel Patel <pzeel1330@gmail.com>
2023-12-13 23:20:26 -0500

    pdf design

--- pages/pdf.php
+++ pages/pdf.php
@@ -5,20 +5,24 @@ class PDF extends FPDF {
     private $headerTitle;
     private $footerTitle;
 
-    // Set header title
+    //header title
     public function setHeaderTitle($title) {
         $this->headerTitle = $title;
     }
 
-    // Set footer title
+    //footer title
     public function setFooterTitle($title) {
         $this->footerTitle = $title;
     }
 
     // Page header
     function Header() {
-        // Set font and color for the header
-        $this->SetFont('Arial', 'B', 12);
+        // background color for the header
+        $this->SetFillColor(240, 79, 36);
+        $this->Rect(0, 0, $this->GetPageWidth(), 20, 'F');
+
+        // font and color for the header
+        $this->SetFont('Arial', 'B', 25);
         $this->SetTextColor(0);
         // Title
         $this->Cell(0, 10, $this->headerTitle, 0, 1, 'C');
@@ -28,39 +32,37 @@ class PDF extends FPDF {
 
     // Page footer
     function Footer() {
-        // Position at 1.5 cm from bottom
+        //background color for the footer
+        $this->SetFillColor(240, 79, 36);
+        $this->Rect(0, $this->GetPageHeight() - 20, $this->GetPageWidth(), 20, 'F');
+
+        // Position
         $this->SetY(-15);
-        // Set font
-        $this->SetFont('Arial', 'I', 8);
+        //font
+        $this->SetFont('Arial', 'I', 15);
         // Title
-        $this->Cell(0, 10, $this->footerTitle . ' - Page ' . $this->PageNo(), 0, 0, 'C');
+        $this->Cell(0, 10, $this->footerTitle ,  0, 0, 'C');
     }
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // ... (Existing code to save data to the database)
+    
 
-    // Create new PDF document
     $pdf = new PDF();
     $pdf->SetTitle('Comics Bookstore');
     $pdf->SetSubject('Form Data PDF');
     $pdf->SetKeywords('Form, Data, PDF');
 
-    // Set custom titles for header and footer
     $pdf->setHeaderTitle('Comics Bookstore');
     $pdf->setFooterTitle('Developed by Duel Ninja - Comics Group - 2023');
 
-    // Add a page
     $pdf->AddPage();
 
-    // Set font
-    $pdf->SetFont('Arial', '', 12);
+    $pdf->SetFont('Arial', '', 15);
 
-    // Retrieve comic details from the form
     $comicTitle = $_POST['comic_title'] ?? '';
     $comicPrice = $_POST['comic_price'] ?? '';
-
-    // Retrieve form data
+    
     $username = $_POST['username'] ?? '';
     $email = $_POST['email'] ?? '';
     $mobile = $_POST['mobile'] ?? '';
@@ -68,23 +70,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $pincode = $_POST['pincode'] ?? '';
     $province = $_POST['province'] ?? '';
 
-    
-
-    // Write comic details to the PDF
     $pdf->Cell(0, 10, 'Comic Title: ' . $comicTitle, 0, 1);
-    $pdf->Cell(0, 10, 'Comic Price: ' . $comicPrice, 0, 1);
+    $pdf->Cell(0, 10, 'Comic Price: $' . $comicPrice, 0, 1);
 
-    // Write content to the PDF
     $pdf->Ln();
-    $pdf->Cell(0, 10, 'Username: ' . $username, 0, 1);
+    $pdf->SetFont('Arial', 'B', 12);
+    $pdf->SetTextColor(255, 0, 0); 
+    $pdf->Cell(0, 10, 'Customer Information:', 0, 1);
+    $pdf->SetFont('Arial', '', 12); 
+    $pdf->SetTextColor(0); 
+    $pdf->Cell(0, 10, 'Name: ' . $username, 0, 1);
     $pdf->Cell(0, 10, 'Email: ' . $email, 0, 1);
     $pdf->Cell(0, 10, 'Mobile: ' . $mobile, 0, 1);
+
+    
+    $pdf->SetFont('Arial', '', 12);
     $pdf->Cell(0, 10, 'Address: ' . $address, 0, 1);
     $pdf->Cell(0, 10, 'Pincode: ' . $pincode, 0, 1);
     $pdf->Cell(0, 10, 'Province: ' . $province, 0, 1);
-
-    
-
 
     // Output PDF as a file
     $pdf->Output('orderdetails.pdf', 'D');
