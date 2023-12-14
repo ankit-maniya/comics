6ada0b644fda8befaebaaa4d6123e5c67e4f060b
Ankit Maniya <66209504+ankit-maniya@users.noreply.github.com>
2023-12-13 22:48:09 -0500

    Genre filter added

--- pages/comics.php
+++ pages/comics.php
@@ -17,37 +17,58 @@ require_once('./components/header.php');
     <header class="bg-light-dark fs-3 text-white py-2 text-center mb-3">
         Comics Store - Comics
     </header>
-    <div class="container-fluid overflow-auto mx-2 mb-2">
-        <ul class="list-group list-group-horizontal flex-nowrap">
 
-            <?php
-            $sql = new DBMaster();
-            $serial_no = 0;
-            $sql->execute("select * from tbl_genres")->forEach(function ($serial_no, $row) {
-                $imgUri = ImageHandler::getImgUri($row['genre_image']);
-                $serial_no++;
-
-                echo "<li class='d-flex align-items-center flex-column'>
-                    <img src='{$imgUri}' alt='{$row['genre_name']}' class='img-height m-3 shadow'>
-                    <span><a href='#' class='text-white fw-bold text-decoration-none'>{$row['genre_name']}</a></span>
-                </li>";
-            });
-            ?>
-        </ul>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="text-white">All Genres</h1>
+                    <ul class="list-group list-group-horizontal flex-nowrap">
+                        <a href='comics.php' class='text-white fw-bold text-decoration-none'>
+                            <li class='d-flex align-items-center flex-column'>
+                                <img src='https://dummyimage.com/512/ffffff/000000&text=ALL' alt='all' class='img-height m-3 shadow'>
+                                <span class='badge text-bg-danger'>All</span>
+                            </li>
+                        </a>
+                        <?php
+                        $sql = new DBMaster();
+                        $serial_no = 0;
+                        $sql_response = $sql->execute("select * from tbl_genres");
+                        $selectedGenreId = isset($_GET['genre_id']) ? $_GET['genre_id'] : null;
+                        $sql_response->forEach(function ($serial_no, $row) use ($selectedGenreId) {
+                            $imgUri = ImageHandler::getImgUri($row['genre_image']);
+                            $serial_no++;
+                            $design =  ($selectedGenreId == $row['genre_id']) ? "text-bg-warning" : "text-bg-primary";
+                            echo "<a href='comics.php?genre_id={$row['genre_id']}' class='text-white fw-bold text-decoration-none'><li class='d-flex align-items-center flex-column'>
+                                <img src='{$imgUri}' alt='{$row['genre_name']}' class='img-height m-3 shadow'>
+                                <span class='badge  {$design}'>{$row['genre_name']}</span>
+                            </li></a>";
+                        });
+                        ?>
+                    </ul>
+            </div>
+        </div>
     </div>
 
-    <div class="container">
+    <div class="container mt-4">
         <div class="row">
-            <!-- Main Content -->
             <div class="col-md-12">
                 <h2 class="text-white">All Comics</h1>
                     <div class="row m-0 g-2 mb-5 pb-4">
                         <?php
                         $sql = new DBMaster();
                         $serial_no = 0;
+                        $params = null;
+
+                        $sql_response = $sql->sqlStatement("select * from tbl_genres JOIN tbl_comics ON tbl_genres.genre_id = tbl_comics.genre_id");
+                        if (isset($_GET['genre_id'])) {
+                            $params = [
+                                'genre_id' => $_GET['genre_id']
+                            ];
+                            $sql_response = $sql->sqlStatement("select * from tbl_genres JOIN tbl_comics ON tbl_genres.genre_id = tbl_comics.genre_id where tbl_genres.genre_id=:genre_id")->params($params);
+                        }
 
                         // Fetch comics with associated genres
-                        $sql->execute("select * from tbl_genres JOIN tbl_comics ON tbl_genres.genre_id = tbl_comics.genre_id")->forEach(function ($serial_no, $row) {
+                        $sql_response->execute()->forEach(function ($serial_no, $row) {
                             $serial_no++;
                             $imgUri = ImageHandler::getImgUri($row['comic_image']);
 
